6f8be32526225107029c2f0f66a6c0318fde18f4
Siddappa Lali <siddappa.lali@gmail.com>
2023-03-08 21:44:53 +0530

    Changes made

---
+++ src/arrays/DemoPage.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class DemoPage {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Samu");
+
+	}
+
+}
--- src/arrays/Tester1.java
+++ src/arrays/Tester1.java
@@ -6,6 +6,7 @@ public class Tester1 {
 	{
 		int[] arr= new int[5];
 		arr[0]=10;
+		
 		arr[1]=20;
 		arr[2]=30;
 		arr[3]=40;

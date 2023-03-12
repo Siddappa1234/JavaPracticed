00d0e057643c2ed0eed5a45a39f8229934a9b469
Siddappa Lali <siddappa.lali@gmail.com>
2023-03-12 09:43:49 +0530

    updated Demopage as per comment

--- src/arrays/DemoPage.java
+++ src/arrays/DemoPage.java
@@ -5,6 +5,8 @@ public class DemoPage {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hi Samu Lali");
+		System.out.println(" I am studing in LKG");
+		System.out.println("Going to public school");
 	}
 
 }

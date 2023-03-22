910a6b65572c99735905974e4d551bc8cdfa36d2
Siddappa Lali <siddappa.lali@gmail.com>
2023-03-22 20:14:37 +0530

    Added Demo2

---
+++ src/abstractclass/DemoB.java
@@ -0,0 +1,12 @@
+package abstractclass;
+
+public class DemoB {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(" Hi ");
+		System.out.println("I am Automation Engineer ");
+
+	}
+
+}

a7789163754d328754d581bc6cedb2e0d5cfb52a
Siddappa Lali <siddappa.lali@gmail.com>
2023-03-25 19:22:28 +0530

    Mail code

--- src/abstraction/Demo2.java
+++ src/abstraction/Demo2.java
@@ -6,6 +6,7 @@ public class Demo2 {
 	{
 		System.out.println("Hi Samu");
 		System.out.println("How are you");
+		System.out.println(" I am fine ");
 
 	}
 
---
+++ src/abstraction/Mail.Java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public class Demo1class {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Shanu ");
+
+	}
+
+}
